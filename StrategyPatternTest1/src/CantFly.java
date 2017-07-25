f21f352d7f3311c2d4f0e9b3685a44813f725eea
Bartek <bartosz.wojtulewicz@gmail.com>
2017-07-25 18:59:43 +0200

    remove

--- StrategyPatternTest1/src/CantFly.java
+++
@@ -1,9 +0,0 @@
-
-public class CantFly implements Flys
-{
-	@Override
-	public String fly() 
-	{
-		return "I can't fly.";
-	}
-}
--- StrategyPatternTest1/src/ItFlys.java
+++
@@ -1,9 +0,0 @@
-
-public class ItFlys implements Flys
-{
-	@Override
-	public String fly() 
-	{
-		return "I'm flying!";
-	}
-}
